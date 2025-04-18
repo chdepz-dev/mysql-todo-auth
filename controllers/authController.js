cc55bdef545d288c1a39e2987a198b74dae6ba4c
Dipak <chdepz@gmail.com>
2025-04-18 14:17:12 +0545

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,11 +18,20 @@ exports.register = (req, res) => {
 
 exports.getLogin = (req, res) => res.sendFile(path.join(__dirname, '../public/html/login.html'));
 
-exports.login = (req, res) =>{
-    const {username, password} = req.body;
-    if(!username || password) return res.status(400).send("all fields are required");
-    
-    db.query("SELECT FROM users WHERE username?", [username],(error, results) =>{
-        
+exports.login = (req, res) => {
+    const { username, password } = req.body;
+    if (!username || password) return res.status(400).send("all fields are required");
+
+    db.query("SELECT FROM users WHERE username?", [username], (error, results) => {
+        if (error) return res.status(500).send("Database error while verifying user");
+        if (!results.length) res.status(401).send("Invalid credentials");
+
+        bcrypt.compare(password, results[0].password, (err, match) => {
+            if (err) return res.status(500).send("server error during password comparison")
+            if (match) {
+                req.session.userId = results[0].id;
+                res.redirect("/todo");
+            } else res.status.send(401).send("Invalid credentials")
+        })
     })
 }
